5f3ab03761ded312030fec58332bd96bcb3665ee
Taehee Yoo <ap420073@gmail.com>
2022-11-09 15:10:29 +0000

    crypto: aria: implement aria-avx2
    aria-avx2 implementation uses AVX2, AES-NI, and GFNI.
    It supports 32way parallel processing.
    So, byteslicing code is changed to support 32way parallel.
    And it exports some aria-avx functions such as encrypt() and decrypt().
    
    There are two main logics, s-box layer and diffusion layer.
    These codes are the same as aria-avx implementation.
    But some instruction are exchanged because they don't support 256bit
    registers.
    Also, AES-NI doesn't support 256bit register.
    So, aesenclast and aesdeclast are used twice like below:
    	vextracti128 $1, ymm0, xmm6;
    	vaesenclast xmm7, xmm0, xmm0;
    	vaesenclast xmm7, xmm6, xmm6;
    	vinserti128 $1, xmm6, ymm0, ymm0;
    
    Benchmark with modprobe tcrypt mode=610 num_mb=8192, i3-12100:
    
    ARIA-AVX2 with GFNI(128bit and 256bit)
        testing speed of multibuffer ecb(aria) (ecb-aria-avx2) encryption
    tcrypt: 1 operation in 2003 cycles (1024 bytes)
    tcrypt: 1 operation in 5867 cycles (4096 bytes)
    tcrypt: 1 operation in 2358 cycles (1024 bytes)
    tcrypt: 1 operation in 7295 cycles (4096 bytes)
        testing speed of multibuffer ecb(aria) (ecb-aria-avx2) decryption
    tcrypt: 1 operation in 2004 cycles (1024 bytes)
    tcrypt: 1 operation in 5956 cycles (4096 bytes)
    tcrypt: 1 operation in 2409 cycles (1024 bytes)
    tcrypt: 1 operation in 7564 cycles (4096 bytes)
    
    ARIA-AVX with GFNI(128bit and 256bit)
        testing speed of multibuffer ecb(aria) (ecb-aria-avx) encryption
    tcrypt: 1 operation in 2761 cycles (1024 bytes)
    tcrypt: 1 operation in 9390 cycles (4096 bytes)
    tcrypt: 1 operation in 3401 cycles (1024 bytes)
    tcrypt: 1 operation in 11876 cycles (4096 bytes)
        testing speed of multibuffer ecb(aria) (ecb-aria-avx) decryption
    tcrypt: 1 operation in 2735 cycles (1024 bytes)
    tcrypt: 1 operation in 9424 cycles (4096 bytes)
    tcrypt: 1 operation in 3369 cycles (1024 bytes)
    tcrypt: 1 operation in 11954 cycles (4096 bytes)
    
    Signed-off-by: Taehee Yoo <ap420073@gmail.com>

--- arch/x86/crypto/aria-avx.h
+++ arch/x86/crypto/aria-avx.h
@@ -7,10 +7,49 @@
 #define ARIA_AESNI_PARALLEL_BLOCKS 16
 #define ARIA_AESNI_PARALLEL_BLOCK_SIZE  (ARIA_BLOCK_SIZE * 16)
 
+#define ARIA_AESNI_AVX2_PARALLEL_BLOCKS 32
+#define ARIA_AESNI_AVX2_PARALLEL_BLOCK_SIZE  (ARIA_BLOCK_SIZE * 32)
+
+
+asmlinkage void aria_aesni_avx_encrypt_16way(const void *ctx, u8 *dst,
+					     const u8 *src);
+asmlinkage void aria_aesni_avx_decrypt_16way(const void *ctx, u8 *dst,
+					     const u8 *src);
+asmlinkage void aria_aesni_avx_ctr_crypt_16way(const void *ctx, u8 *dst,
+					       const u8 *src,
+					       u8 *keystream, u8 *iv);
+asmlinkage void aria_aesni_avx_gfni_encrypt_16way(const void *ctx, u8 *dst,
+						  const u8 *src);
+asmlinkage void aria_aesni_avx_gfni_decrypt_16way(const void *ctx, u8 *dst,
+						  const u8 *src);
+asmlinkage void aria_aesni_avx_gfni_ctr_crypt_16way(const void *ctx, u8 *dst,
+						    const u8 *src,
+						    u8 *keystream, u8 *iv);
+
+asmlinkage void aria_aesni_avx2_encrypt_32way(const void *ctx, u8 *dst,
+					      const u8 *src);
+asmlinkage void aria_aesni_avx2_decrypt_32way(const void *ctx, u8 *dst,
+					      const u8 *src);
+asmlinkage void aria_aesni_avx2_ctr_crypt_32way(const void *ctx, u8 *dst,
+						const u8 *src,
+						u8 *keystream, u8 *iv);
+asmlinkage void aria_aesni_avx2_gfni_encrypt_32way(const void *ctx, u8 *dst,
+						   const u8 *src);
+asmlinkage void aria_aesni_avx2_gfni_decrypt_32way(const void *ctx, u8 *dst,
+						   const u8 *src);
+asmlinkage void aria_aesni_avx2_gfni_ctr_crypt_32way(const void *ctx, u8 *dst,
+						     const u8 *src,
+						     u8 *keystream, u8 *iv);
+
 struct aria_avx_ops {
 	void (*aria_encrypt_16way)(const void *ctx, u8 *dst, const u8 *src);
 	void (*aria_decrypt_16way)(const void *ctx, u8 *dst, const u8 *src);
 	void (*aria_ctr_crypt_16way)(const void *ctx, u8 *dst, const u8 *src,
 				     u8 *keystream, u8 *iv);
+	void (*aria_encrypt_32way)(const void *ctx, u8 *dst, const u8 *src);
+	void (*aria_decrypt_32way)(const void *ctx, u8 *dst, const u8 *src);
+	void (*aria_ctr_crypt_32way)(const void *ctx, u8 *dst, const u8 *src,
+				     u8 *keystream, u8 *iv);
+
 };
 #endif
